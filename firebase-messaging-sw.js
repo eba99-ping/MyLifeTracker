0e3da1ae8d23b2163ca345e1e4d77c1474340ebe
eba99-ping <elbegbayarelbegbayr3@gmail.com>
2026-08-20 21:45:42 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,11 +1,9 @@
-// firebase-messaging-sw.js
-
 importScripts(
-  "https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js"
+"https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js"
 );
 
 importScripts(
-  "https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js"
+"https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js"
 );
 
 
@@ -13,7 +11,6 @@ firebase.initializeApp({
   apiKey: "AIzaSyD2IpjP7BGUikR5cc7L2DvW0PYbPPQTPNw",
   authDomain: "my-life-tracker-6c19b.firebaseapp.com",
   projectId: "my-life-tracker-6c19b",
-  storageBucket: "my-life-tracker-6c19b.firebasestorage.app",
   messagingSenderId: "932788931683",
   appId: "1:932788931683:web:d19934806548790d39bc56"
 });
@@ -22,23 +19,13 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 
-messaging.onBackgroundMessage(
-  (payload)=>{
+messaging.onBackgroundMessage((payload)=>{
+  
+  self.registration.showNotification(
+    payload.notification.title || "My Life Tracker",
+    {
+      body: payload.notification.body || "Reminder"
+    }
+  );
 
-    console.log(
-      "Background message:",
-      payload
-    );
-
-
-    self.registration.showNotification(
-      payload.notification.title || "My Life Tracker",
-      {
-        body:
-        payload.notification.body ||
-        "Reminder"
-      }
-    );
-
-  }
-);
+});
